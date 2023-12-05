12758ef97741b7c08184e66b6baa8dce993e8010
ShauryaJ1 <shaurya.s.jain@gmail.com>
2023-12-05 09:05:42 -0500

    sql example

--- node_stuff/express_templates/mysql_lab/index.js
+++ node_stuff/express_templates/mysql_lab/index.js
@@ -5,4 +5,18 @@ app.set('view engine','ejs')
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('database.db');
 
+app.get('/',(req,res)=>{
+    res.render('form')
+})
 
+app.get('/heroStats',(req,res)=>{
+  res.render('heroStats')
+})
+
+const listener = app.listen(
+    process.env.PORT || 8080,
+    process.env.HOST || "0.0.0.0",
+    function() {
+      console.log("Express server started");
+    }
+)
