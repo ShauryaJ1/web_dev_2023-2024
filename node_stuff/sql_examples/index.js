fae813b1932845eeecb2d885f6432545eeb7bfb8
ShauryaJ1 <shaurya.s.jain@gmail.com>
2023-12-12 10:05:42 -0500

    sql functionality

--- node_stuff/sql_examples/index.js
+++ node_stuff/sql_examples/index.js
@@ -10,7 +10,9 @@ function sqlPromise(query, params =[]) {
     })
   }
 
-let sql = `SELECT * FROM assigned_quests`
+let assigned_equipment_query = `SELECT * FROM assigned_equipment`
+let assigned_quests_query = `SELECT * FROM assigned_quests`
+
 // let sql2 = `SELECT * FROM equipment WHERE equipment_id = ?`
 // let sql3 = `SELECT * FROM quest WHERE quest_id = ?`
 // let params1 = ['Henrik']
@@ -39,8 +41,8 @@ app.set('view engine','ejs')
 // const sqlite3 = require('sqlite3').verbose()
 // const db = new sqlite3.Database('database.db');
 
-app.get('/questForm',(req,res)=>{
-    res.render('questForm')
+app.get('/Form',(req,res)=>{
+    res.render('Form')
 })
 
 app.get('/heroStats',async (req,res)=>{
@@ -49,6 +51,7 @@ app.get('/heroStats',async (req,res)=>{
     character = 'NA'
     let query1 = `INSERT INTO assigned_equipment VALUES (?,?)`
     let query2 = `INSERT INTO assigned_quests VALUES (?,?)`
+    let query3 = `SELECT * FROM assigned_quests INNER JOIN assigned_equipment ON assigned_equipment.c_name = assigned_quests.c_name INNER JOIN characters ON characters.c_name = assigned_quests.c_name`
     result = {}
     if('character' in req.query){
         character = req.query.character
@@ -56,24 +59,29 @@ app.get('/heroStats',async (req,res)=>{
     if ('quests' in req.query){
 
         quests = req.query.quests;
-        result = await sqlPromise(query2,[character,quests])
-        db.all(sql,  (err, rows) => {
-            //     console.log(err)
-                console.log(rows);
-            });
+        result1 = await sqlPromise(query2,[character,quests])
+        result1 = await sqlPromise(assigned_quests_query)
+        console.log(result1)
+        
     }
     if('equipment' in req.query){
         equipment = req.query.equipment
+        result2 = await sqlPromise(query1,[character,equipment])
+        result2 = await sqlPromise(assigned_equipment_query)
+        console.log(result2)
     }
     
-    const render_dict = {'quests':quests,'equipment':equipment,'character':character,'result':result}
+    
+    result = await sqlPromise(query3)
 
+    console.log(result)
+    const render_dict = {'quests':quests,'equipment':equipment,'character':character,'result':result}
     res.render('heroStats',render_dict)
 })
 
-app.get('/equipmentForm',(req,res)=>{
-    res.render('equipmentForm')
-})
+// app.get('/equipmentForm',(req,res)=>{
+//     res.render('equipmentForm')
+// })
 
 const listener = app.listen(
     process.env.PORT || 8080,
