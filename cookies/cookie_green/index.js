782591dac1295dbe91452cba7b0cfd6b1dad9d4f
ShauryaJ1 <shaurya.s.jain@gmail.com>
2024-01-02 09:44:43 -0500

    first green done

---
+++ cookies/cookie_green/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+app.set('view engine', 'ejs')
+
+app.get('/setcookie', (req, res) => {
+
+    // Set the 'username' cookie to expire in 7 days (in ms)
+    const expirationDate = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); 
+
+    let shauryaCookie = 'shaurya'
+    let cookieData = 'myVal'
+    
+    res.cookie(shauryaCookie, cookieData, {
+        expires: expirationDate,
+    });
+
+    res.render('cookie')
+});
+
+
+const listener = app.listen(
+    process.env.PORT || 8080,
+    process.env.HOST || "0.0.0.0",
+    function() {
+      console.log("Express server started");
+    })
